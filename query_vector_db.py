e4bc0b87561684cda653190ccbd0e6473012687d
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-03-25 13:35:26 +0200

    feat(query_vector_db.py): finished query script

--- query_vector_db.py
+++ query_vector_db.py
@@ -1,14 +1,40 @@
 """
-Example usage:
+Interactive querying of the vector database from the command line
 
-    python query_vector_db.py \
-            --query "What are rats good at?" \
-            --top_k 3
+Example usage:
+    $ python query_vector_db.py --help
+    $ python query_vector_db.py --top_k 3
+
+Example queries:
+    "Do rats carry disease?"
+    "Can a rat smile?"
 """
 
-
 import argparse
+import json
 
-from create_vector_db import chroma_collection
+from create_vector_db import chromadb_collection
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-k",
+    "--top_k",
+    help="Number of matches to return from the database for each query",
+    type=int,
+    required=True,
+)
+args = parser.parse_args()
 
+while True:
+    print("Please enter a query: ")
+    print('(enter "exit" or use <ctrl+C> to terminate this program)')
+    user_query = input()
+    if user_query == "exit":
+        break
+    query_results = chromadb_collection.query(
+        query_texts=[user_query], n_results=args.top_k
+    )
+    print(json.dumps(query_results, indent=4))
+    _ = input("Press enter to continue")
+    if _ == "exit":
+        break
